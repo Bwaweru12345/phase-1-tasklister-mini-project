4b9503d21f41b55607f12fdad55973c015603fd9
Bwaweru12345 <111865501+Bwaweru12345@users.noreply.github.com>
2023-03-30 15:40:18 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let listen = document.querySelector('form')
+  listen.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTasks(e.target.task.value);
+    
+
+  })
+
+   
+})
+
+function handleTasks(task){
+  let p = document.createElement('p')
+  p.textContent = task;
+  document.querySelector('#list').appendChild(p)
+
+}
+
+class task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
+
+
